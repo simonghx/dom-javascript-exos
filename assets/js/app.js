71caae501375f9eabd0912ce5771da98b3e2a88a
simonghx <simongheux@gmail.com>
2018-03-29 16:25:09 +0200

    Fin de l'exo 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -190,4 +190,34 @@ boutonExo4.addEventListener("click", changeCube);
 
 //EXO 5
 
+// récuper ma div panier
+let panier = document.querySelector("#exo5 .panier");
+
+//récupérer mon bouton
+let boutonOeuf = document.querySelector("#exo5 .add-egg");
+
+let imgOeuf = document.createElement("img");
+
+//créer la fonction
+let createOeuf = function (event) {
+
+  event.preventDefault();
+  // créer une div
+  let imgOeuf = document.createElement("img");
+
+  // mettre une id à cette nouvelle div
+  imgOeuf.setAttribute("src", "./assets/img/egg.png");
+
+  //mettre une taille à mon image
+  imgOeuf.setAttribute("width", "50px");
+
+  //mettre dans la div
+  panier.appendChild(imgOeuf);
+
+};
+
+// créer l'écouteur d'évènement
+boutonOeuf.addEventListener("click", createOeuf);
+
+
 
