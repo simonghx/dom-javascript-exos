31851c10c908d1614d29119f878318d9565ee016
simonghx <simongheux@gmail.com>
2018-04-05 18:45:06 +0200

    Commencement des fonctions sur icones

--- assets/js/exo7.js
+++ assets/js/exo7.js
@@ -10,7 +10,37 @@ let groupeList = document.querySelector("#exo7 .list-group");
 // récupérer les boutons
 let btnTags = document.querySelectorAll("#exo7 .nav-todo .btn");
 
+let activation;
+let icnSuppr;
+let icnModif;
+let icnCheck;
+let listItem;
+
 // functions
+
+let supprimer = function (event) {
+
+};
+
+let modifier = function () {
+
+};
+
+let checker = function (event) {
+  activation = listItem.getAttribute("data-activation");
+  // empêche le refresh par défaut de la page
+  event.preventDefault();
+
+  if (activation == "0") {
+    //barrer le texte
+    listItem.classList.add("line");
+    activation = "1";
+    console.log("hello");
+  } else {
+    listItem.classList.remove("line");
+    activation = "0";
+  }
+};
 
 let createTask = function (event) {
 
@@ -24,18 +54,28 @@ let createTask = function (event) {
   } else {
 
     //créer un élément de liste
-    let listItem = document.createElement("li");
+    listItem = document.createElement("li");
 
     // ajouter une class à mon item
-    listItem.classList.add("list-group-item", "row");
+    listItem.classList.add("list-group-item", "d-flex", "row");
     // ajouter une valeur
-    listItem.innerHTML = '<div class="col-6 text-left d-inline-block">' + inputTask.value + '</div><div class="icones col-6 text-right d-inline-block"><i class="far fa-window-close mx-1"></i><i class="fas fa-check mx-1"></i><i class="far fa-edit mx-1"></i></div>';
+    listItem.innerHTML = '<div class="text-item col-6 text-left">' + inputTask.value + '</div><div class="icones col-6 text-right"><a href="" class="delete" data-activation="0"><i class="far fa-window-close mx-1"></i></a><a class="check" data-activation="0" href=""><i class="fas fa-check mx-1"></i></a><a class="edit" data-activation="0" href=""><i class="far fa-edit mx-1"></i></a></div>';
 
     //supprimer le contenu de l'input
     inputTask.value = "";
 
     // ajouter le li dans la liste
     groupeList.appendChild(listItem);
+
+    // récupérer les icones
+    icnSuppr = listItem.querySelector("#exo7 .delete");
+    icnModif = listItem.querySelector("#exo7 .edit");
+    icnCheck = listItem.querySelector("#exo7 .check");
+
+    // ecouteur d'évènements icones
+    icnSuppr.addEventListener("click", supprimer);
+    icnModif.addEventListener("click", modifier);
+    icnCheck.addEventListener("click", checker);
 
 
   }
