f2960ef63a165dd1d17e669a9659bd357efec73c
simonghx <simongheux@gmail.com>
2018-04-06 11:04:04 +0200

    Fin de la fonction checker

--- assets/js/exo7.js
+++ assets/js/exo7.js
@@ -10,35 +10,49 @@ let groupeList = document.querySelector("#exo7 .list-group");
 // récupérer les boutons
 let btnTags = document.querySelectorAll("#exo7 .nav-todo .btn");
 
-let activation;
 let icnSuppr;
 let icnModif;
 let icnCheck;
 let listItem;
+let position = 0;
+// let activation;
 
 // functions
 
 let supprimer = function (event) {
+  // empêche le refresh par défaut de la page
+  event.preventDefault();
 
 };
 
-let modifier = function () {
+let modifier = function (event) {
+  // empêche le refresh par défaut de la page
+  event.preventDefault();
 
 };
 
 let checker = function (event) {
-  activation = listItem.getAttribute("data-activation");
   // empêche le refresh par défaut de la page
   event.preventDefault();
 
-  if (activation == "0") {
+  // récupérer l'activation de la target
+  let activation = event.target.getAttribute("data-activation");
+
+  // récupérer le parent le plus proche de mon event.target ayant la class list-group-item
+  let parentTest = event.target.closest(".list-group-item");
+
+  if (activation == 0) {
     //barrer le texte
-    listItem.classList.add("line");
-    activation = "1";
-    console.log("hello");
-  } else {
-    listItem.classList.remove("line");
-    activation = "0";
+    parentTest.classList.add("line");
+    activation = 1;
+    event.target.setAttribute("data-activation", activation);
+
+  }
+  else {
+    // retirer le texte barré
+    parentTest.classList.remove("line");
+    activation = 0;
+    event.target.setAttribute("data-activation", activation);
   }
 };
 
@@ -56,10 +70,16 @@ let createTask = function (event) {
     //créer un élément de liste
     listItem = document.createElement("li");
 
+    //ajouter un ID
+    listItem.id = "list-" + position;
+    position++;
+    console.log(listItem);
+    console.log(position);
+
     // ajouter une class à mon item
     listItem.classList.add("list-group-item", "d-flex", "row");
     // ajouter une valeur
-    listItem.innerHTML = '<div class="text-item col-6 text-left">' + inputTask.value + '</div><div class="icones col-6 text-right"><a href="" class="delete" data-activation="0"><i class="far fa-window-close mx-1"></i></a><a class="check" data-activation="0" href=""><i class="fas fa-check mx-1"></i></a><a class="edit" data-activation="0" href=""><i class="far fa-edit mx-1"></i></a></div>';
+    listItem.innerHTML = '<div class="text-item col-6 text-left">' + inputTask.value + '</div><div class="icones col-6 text-right"><a href="" class="delete btn btn-light" data-activation="0"><i class="far fa-window-close mx-1"></i></a><a class="check btn btn-light" href="" data-activation="0"><i class="fas fa-check mx-1"></i></a><a class="edit btn btn-light" href="" data-activation="0"><i class="far fa-edit mx-1"></i></a></div>';
 
     //supprimer le contenu de l'input
     inputTask.value = "";
@@ -68,9 +88,10 @@ let createTask = function (event) {
     groupeList.appendChild(listItem);
 
     // récupérer les icones
-    icnSuppr = listItem.querySelector("#exo7 .delete");
-    icnModif = listItem.querySelector("#exo7 .edit");
-    icnCheck = listItem.querySelector("#exo7 .check");
+    icnSuppr = listItem.querySelector("#exo7 a.delete");
+    icnModif = listItem.querySelector("#exo7 a.edit");
+    icnCheck = listItem.querySelector("#exo7 a.check");
+
 
     // ecouteur d'évènements icones
     icnSuppr.addEventListener("click", supprimer);
